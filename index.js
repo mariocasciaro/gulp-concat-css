0b8c9bf1233be5c0366b18521dc86132eaa57f7e
Mario Casciaro <mariocasciaro@gmail.com>
2014-06-11 09:35:45 +0100

    Fix problem with url rewrites in nested imports

--- index.js
+++ index.js
@@ -8,39 +8,46 @@ var gutil = require('gulp-util'),
 
 
 module.exports = function(destFile) {
-  var destDir = path.dirname(destFile);
   var buffer = [];
   var firstFile = null;
-  
+  var destDir = path.dirname(destFile);
+
   return through.obj(function(file, enc, cb) {
     if (file.isStream()) {
       this.emit('error', new gutil.PluginError('gulp-concat-css', 'Streaming not supported'));
       return cb();
     }
-    
+
     if(!firstFile) {
       firstFile = file;
     }
 
+    function urlPlugin(file) {
+      return rework.url(function(url) {
+        if(isUrl(url) || path.extname(url) === '.css' || path.resolve(url) === url) {
+          return url;
+        }
+        var resourceAbsUrl = path.relative(file.base, path.resolve(path.dirname(file.path), url));
+        resourceAbsUrl = path.relative(destDir, resourceAbsUrl);
+        //not all systems use forward slash as path separator
+        //this is required by urls.
+        if(path.sep === '\\'){
+          //replace with forward slash
+          resourceAbsUrl = resourceAbsUrl.replace(/\\/g, '/');
+        }
+        return resourceAbsUrl;
+      });
+    }
+
     try {
       var processedCss = rework(String(file.contents), 'utf-8')
+        .use(urlPlugin(file))
         .use(reworkImporter({
           path: file.path,
-          base: file.base
-        }))
-        .use(rework.url(function(url){
-          if(isUrl(url)) {
-            return url;
+          base: file.base,
+          preProcess: function(ast, opts) {
+            return ast.use(urlPlugin(opts));
           }
-          var resourceAbsUrl = path.relative(file.base, path.resolve(path.dirname(file.path), url));
-          resourceAbsUrl = path.relative(destDir, resourceAbsUrl);
-          //not all systems use forward slash as path separator
-          //this is required by urls.
-          if(path.sep === '\\'){
-            //replace with forward slash
-            resourceAbsUrl = resourceAbsUrl.replace(/\\/g, '/');
-          }
-          return resourceAbsUrl;
         }))
         .toString();
     } catch(err) {
